277680f205c68097d0a1c20d7779eaafa5c35bfc
Oleksandr Reinskyi <112425540+Dalbaebb@users.noreply.github.com>
2024-08-23 19:17:42 +0300

    Commited first lecture homework

---
+++ Homeworks/1/Calculator/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+
+namespace Calculator
+    /*
+     Pseudocode:
+    It will be a calculator that firstly takes 2 numbers, performs an operation and then waits for another user's input 
+    with the first number being the result of the previous operation
+    
+    Must implement a Calculator class with
+    - public string array with first and second numbers that are set to null
+    - public char operation
+    - public float function that will return an output based on operation
+    
+    Helper functions:
+    - askUser function that will ask a question and get the output
+    - Validate function that will validate the number's inputs 
+
+    While loop until the user hits Ctr+C 
+     */
+{
+   class Calculator
+   {
+        public Calculator() {
+        
+        }
+        public string[] numbers = new string[2] { null, null };
+        public string operation = null; 
+
+
+
+        public double Calculate()
+        {
+            double number1 = Convert.ToDouble(this.numbers[0]);
+            double number2 = Convert.ToDouble(this.numbers[1]);
+            switch (this.operation)
+            {
+                case "*":
+                    return number1 * number2;
+                case "-":
+                    return number1 - number2;
+                case "+":
+                    return number1 + number2;
+                case "/":   
+                    if (number2 ==0) {
+                        throw new Exception("Cannot divide by 0!");
+                    }
+                    return number1 / number2;
+                case "sqrt":
+                    if (number1 < 0)
+                    {
+                        throw new Exception("Cannot get the square root of a less than 0 value (number 1)!");
+                    }
+                    return Math.Sqrt(number1);
+                case "pow":
+                    if (number2 < 0) {
+                        throw new Exception("Cannot raise to the power that is less than 0!");
+                    }
+                    return Math.Pow(number1, number2);
+                default:
+                    throw new InvalidOperationException("Please insert a valid operation!");
+            }
+        }
+        
+   }
+
+    internal class Program
+    {
+        public static string AskUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static bool Validate(string str)
+        {
+            return Regex.IsMatch(str, @"^((\-)?\d*,?\d+)$");
+        }
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            string startingMessage = @"Welcome to the calculator! 
+You will be asked to insert operation and 2 numbers. 
+After each operation the result will be stored as the 1 number.So you can continue working with it.
+The list of supported operations:
+    1. * 
+    2. +
+    3. -
+    4. /
+    5. pow - raising the 1 number to the 2 number
+    6. sqrt - getting the square root of the 1 number
+Enjoy!
+P.s.: You can exit the calculator by pressing Ctr+C.
+                                    ";
+            Console.WriteLine(startingMessage);
+            while (true) {
+                try
+                {
+                    int fin = 2; 
+                    calculator.operation = AskUser("Insert an operation: ");
+                    if(calculator.operation == "sqrt")
+                    {
+                        fin = 1;
+                    }
+                    for(int i = 0; i < fin; i++)
+                    {
+                        if (calculator.numbers[i] == null)
+                        {
+                            string answer = AskUser($"Insert the {i+1} number: ");
+                            while (Validate(answer) == false)
+                            {
+                                answer = AskUser($"Insert the {i+1} number: ");
+                            }
+                            calculator.numbers[i] = answer;
+                        }
+                    }
+
+                    string result = Convert.ToString(calculator.Calculate());
+                    calculator.numbers[0] = result;
+                    Console.WriteLine("Result: " + result);
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Error occured: "+e.Message);
+                }
+                finally
+                {
+                    calculator.operation = null;
+                    calculator.numbers[1] = null;
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/1/Converter/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+
+    internal class Program
+    {
+        public static string AskUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static char AskTemperature(string message) {
+            return char.ToUpper(AskUser(message)[0]);
+        }
+        public static bool CheckTemperature(char temp)
+        {
+            return (temp == 'F' || temp == 'C' || temp == 'K');
+        }
+        public static bool CheckValue(string input) {
+            return Regex.IsMatch(input, @"^((\-)?\d*,?\d+)$");
+        }
+        public static float ConvertTemperature(char from, char to, float value)
+        {
+            if (from == 'C' && to == 'F')
+            {
+                return value * 1.8f + 32;
+            }
+            else if (from == 'F' && to == 'C')
+            {
+
+                return (value - 32f) / 1.8f;
+            }
+            else if (from == 'K' && to == 'C')
+            {
+                return value - 273.15f;
+            }
+            else if (from == 'C' && to == 'K')
+            {
+                return value + 273.15f;
+            }else if(from=='C' && to =='C')
+            {
+                return value;
+            }
+            throw (new Exception("Invalid values passed to ConvertTemperature Function! "));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to temperature converter!\nValid inputs for temperatures are:\n \t1) C - Celsius\n \t2) F - Fahrenheit\n \t3) K - Kelvin");
+
+            char typeFrom = AskTemperature("Insert a temperature type of a value that must be converted: ");
+            char typeTo = AskTemperature("Insert a temperature type to which the value must be coverted: ");
+
+            if (!CheckTemperature(typeFrom) || !CheckTemperature(typeTo) || (typeFrom == typeTo)) {
+                Console.WriteLine("Error: the user inserted incorrect type of temperature or temperature types are the same! ");
+                return;
+            }
+
+
+            string value = AskUser("Insert a value: ");
+            if (!CheckValue(value)) {
+                Console.WriteLine("Error: insert a correct number! ");
+                return;
+            }
+
+
+            float toCelsius = ConvertTemperature(typeFrom, 'C', (float)Convert.ToDouble(value));
+            if (toCelsius < -273.15f)
+            {
+                Console.WriteLine("Error: the input value exceeded the absolute zero threshold! ");
+                return;
+            }
+
+
+            float result = ConvertTemperature('C', typeTo, toCelsius);
+            Console.WriteLine($"Result: {result}");
+
+        }
+    }
+}
