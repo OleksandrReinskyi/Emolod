1dfa73c6036454f7332590bf3abc2cd219fcb631
Oleksandr Reinskyi <112425540+OleksandrReinskyi@users.noreply.github.com>
2024-09-15 15:27:54 +0300

    4

---
+++ Homeworks/4/1/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp18
+{
+    // Entity
+    class PhoneContact
+    {
+        //private public protected
+        private static int autoInc = 1;
+        private int id = 0;
+        private string name = "";
+        private string phone = "";
+
+        public PhoneContact()
+        {
+
+        }
+
+        public PhoneContact(string name, string phone)
+        {
+            this.id = autoInc++;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName() => this.name;
+
+        public void setPhone(string phone)
+        {
+            this.phone = phone;
+        }
+        public string getPhone() => this.phone;
+    }
+
+    //Service
+    class PhoneBook
+    {
+        private List<PhoneContact> contacts = null;
+
+
+        public PhoneBook()
+        {
+            contacts = new List<PhoneContact>();
+
+        }
+
+
+        public PhoneContact create(string name, string phone) // phone validation
+        {
+            validateContact(name, phone);
+            contacts.Add(new PhoneContact(name, phone));
+            return contacts.Last();
+        }
+
+        public List<PhoneContact> getContacts() { return contacts; }
+
+        public PhoneContact getContact(int id) // return exception if not found
+        {
+            PhoneContact contact = contacts.Find(value => value.getId() == id);
+            if(contact == null)
+            {
+                throw new Exception("Error: the contact with that id doesn't exist!");
+            }
+            return contact;
+        }
+
+        public PhoneContact update(int id, string name, string phone)
+        {
+            PhoneContact contact = idValidation(id);
+            validateContact(name, phone);
+
+            contact.setName(name);
+            contact.setPhone(phone);
+
+            return contact;
+        }
+
+        public bool remove(int id)
+        {
+            PhoneContact contact = idValidation(id);
+
+            return contacts.Remove(contact);
+        }
+
+        private bool phoneValidation(string phone)
+        {
+            phone = phone.Trim();
+            if (phone.Length < 7)
+            {
+                return false;
+            }
+            string reg = @"^\+?(38)?0{1}\d+$";
+            return Regex.IsMatch(phone, reg);
+        }
+
+        private bool existenceValidation(string name, string phone)
+        {
+            foreach (PhoneContact item in contacts)
+            {
+                if (item.getName() == name || item.getPhone() == phone)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private PhoneContact idValidation(int id)
+        {
+            PhoneContact contact = contacts.Find(value => value.getId() == id);
+
+            if (contact == null)
+                throw new Exception("Error: contact with that id doesn't exist!");
+            return contact;
+        }
+        private void validateContact(string name, string phone)
+        {
+            phone = phone.Trim();
+
+            if (name.Trim().Length < 2)
+            {
+                throw new Exception("Error: short name!");
+            }
+            if (!phoneValidation(phone))
+            {
+                throw new Exception("Error: invalid phone number!");
+            }
+
+            if (existenceValidation(name, phone))
+            {
+                throw new Exception("Error: contact already exists!");
+            }
+        }
+
+
+    }
+
+
+    class Helper
+    {
+        public static string askUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> operations = new List<string>() { "Create", "Update", "Remove", "Get Contact", "Show All", "Exit" };
+            Console.WriteLine("Welcome to the Phone Book application! The list of supported operations:");
+            for (int i = 0; i < operations.Count; i++)
+            {
+                Console.WriteLine($"\t{i} - {operations[i]}");
+            }
+            PhoneBook phoneBook = new PhoneBook();
+            while (true)
+            {
+                try
+                { 
+                    int operation = Convert.ToInt32(Helper.askUser("Choose an operation: "));
+
+                    if (!(operation>=0 && operation < operations.Count)) 
+                    {
+                        throw new Exception("Error: invalid operation!");
+                    }
+
+                    List<string> messages = new List<string>() {"Insert an id: ", "Insert a name: ", "Insert a phone: "};
+
+                    switch (operation)
+                    {
+                        case 0:
+                            phoneBook.create(Helper.askUser(messages[1]), Helper.askUser(messages[2]));
+                            break;
+                        
+                        case 4:
+                            foreach(PhoneContact item in phoneBook.getContacts())
+                            {
+                                Console.WriteLine($"{item.getId()}) {item.getName()}: {item.getPhone()}");
+                            }
+                            break;
+                        case 5:
+                            return;
+
+                        default:
+                            int id = Convert.ToInt32(Helper.askUser(messages[0]));
+                            switch (operation)
+                            {
+                                case 1:
+                                    phoneBook.update(id, Helper.askUser(messages[1]), Helper.askUser(messages[2]));
+                                    break;
+                                case 2:
+                                    phoneBook.remove(id);
+                                    break;
+                                case 3:
+                                    PhoneContact contact = phoneBook.getContact(id);
+                                    Console.WriteLine($"{contact.getId()}) {contact.getName()}: {contact.getPhone()}");
+                                    break;
+                            }       
+                            break;
+                    }
+
+                }
+                catch (Exception e) { 
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
---
+++ Homeworks/4/2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+1) Class Product:
+-- Price
+-- Quantity 
+-- Name
+-- Set quanitity func
+2) Class Shop:
+-- List with products
+-- Buy function 
+--- Check if that quantity is present
+--- Subtract from 
+3) While loop
+-- Print the catalogue
+-- Ask to choose the product and insert the cash
+
+ */
+
+
+namespace _2
+{
+    class Product
+    {
+        private string _name;
+        private int _quantity;
+        private int _price;
+
+        public string name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+        public int quantity
+        {
+            get { return this._quantity; }
+            set { this._quantity = value; }
+        }
+        public int price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+        public Product(string name,int price, int quantity) {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+    }
+
+    class Shop
+    {
+        private List<Product> products = null;
+        public Shop(List<Product> products)
+        {
+            this.products = products;
+        }
+        public List<Product> getProducts() {
+            return products;
+        }
+        public void addProduct(Product item) {
+            products.Add(item);
+        }
+        public List<string> buyProduct(string name, int cash)
+        {
+            Product product = products.Find(item=>item.name==name.ToLower());
+            if(product == null)
+            {
+                throw new Exception("Invalid product name!");
+            }
+            int quantity = (cash * 100) / (product.price * 100);
+            if(quantity > product.quantity)
+            {
+                throw new Exception("Not enough products in the shop!");
+            }
+            if(quantity < 0)
+            {
+                throw new Exception("Not enough money provided!");
+            }
+            product.quantity = product.quantity - quantity;
+            int cashLeft = ((cash*100) - (product.price*quantity*100))/100;
+            return new List<string>() { product.name, Convert.ToString(quantity), Convert.ToString(cashLeft)};
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static public string askUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>() { new Product("soap",12,34), new Product("toothpaste", 52, 56) , new Product("shampoo", 70, 12)};
+            Console.WriteLine("Welcome to the Shop application.");
+
+            Shop shop = new Shop(products);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Available products: ");
+                    foreach (Product item in shop.getProducts())
+                    {
+                        Console.WriteLine($"{item.name}: {item.quantity}x{item.price} ");
+                    }
+                    string name = askUser("\nInsert the name of the product: ");
+                    int money = Convert.ToInt32(askUser("Insert the available money: "));
+
+                    List<string> answer = shop.buyProduct(name,money);
+
+                    Console.WriteLine($"You bought {answer[0]}x{answer[1]}. And your change is: {answer[2]}");
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
